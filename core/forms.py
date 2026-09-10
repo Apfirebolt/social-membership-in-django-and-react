dcc6d2c5c660a1a5b996e063f17a50435bbf9e40
Apfirebolt <aspper20@gmail.com>
2026-09-10 10:57:00 +0530

    more UI upgrades added

--- core/forms.py
+++ core/forms.py
@@ -9,9 +9,9 @@ class GroupForm(forms.ModelForm):
         'name_required': ("Name is a required field."),
     }
     name = forms.CharField(label=("Name"),
-                                widget=forms.TextInput(attrs={'class': 'block w-full py-3 px-2 shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md'}))
+                                widget=forms.TextInput(attrs={'class': 'block w-full border border-blue-700 py-3 px-2 shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md'}))
     description = forms.CharField(label=("Description"),
-                                widget=forms.Textarea(attrs={'class': 'block w-full py-3 px-2 shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md'}),
+                                widget=forms.Textarea(attrs={'class': 'block w-full border border-blue-700 py-3 px-2 shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md'}),
                                 )
     def clean_name(self):
         name = self.cleaned_data.get('name')
@@ -35,11 +35,11 @@ class PlotForm(forms.ModelForm):
         'description_required': ("Description is a required field."),
     }
     location = forms.CharField(label=("Name"),
-                                widget=forms.TextInput(attrs={'class': 'block w-full py-3 px-2 shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md'}))
+                                widget=forms.TextInput(attrs={'class': 'block w-full border border-blue-700 py-3 px-2 shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md'}))
     price = forms.CharField(label=("Name"),
-                                widget=forms.TextInput(attrs={'class': 'block w-full py-3 px-2 shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md'}))
+                                widget=forms.TextInput(attrs={'class': 'block w-full border border-blue-700 py-3 px-2 shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md'}))
     description = forms.CharField(label=("Description"),
-                                widget=forms.Textarea(attrs={'class': 'block w-full py-3 px-2 shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md'}),
+                                widget=forms.Textarea(attrs={'class': 'block w-full border border-blue-700 py-3 px-2 shadow-sm sm:text-sm focus:ring-grape-500 focus:border-grape-500 border-gray-300 rounded-md'}),
                                 )
     
     class Meta:
